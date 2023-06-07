b3fefb2efef8df9f33cf368c1f10e4ae91860214
GustavoSantana <211038235@aluno.unb.br>
2023-06-07 15:48:44 -0300

    documentacao

--- rainhas.cpp
+++ rainhas.cpp
@@ -1,18 +1,21 @@
 #include <bits/stdc++.h>
+#include <vector>
+#include <string>
 #include "rainhas.hpp"
 
 using namespace std;
+
 bool formatacao(string teste) {
     string line;
-    ifstream arquivo (teste);
-    if (arquivo.is_open()){
+    ifstream arquivo(teste);
+    if (arquivo.is_open()) {
         int i = 0;
         int qntd_rainhas = 0;
-        while (! arquivo.eof()) {
-            if (i == 8){
+        while (!arquivo.eof()) {
+            if (i == 8) {
                 return true;
             }
-            getline(arquivo,line);
+            getline(arquivo, line);
             if (line.length() != 8) {
                 return true;
             } else {
@@ -38,45 +41,42 @@ int** matriz(string teste) {
     for (int i = 0; i < 8; i++) {
         ums[i]= (int*)malloc(sizeof(int)*2);
     }
-    
-    
 
     ifstream arquivo (teste);
     if (arquivo.is_open()) {
         int y = 0, x = 0;
-        while (! arquivo.eof()) {
-            getline(arquivo,line);
+        while (!arquivo.eof()) {
+            getline(arquivo, line);
             for (int i = 0; i < 8; i++) {
-                if (line[i]=='1') {
+                if (line[i] == '1') {
                     ums[x][0] = i + 1;
                     ums[x][1] = y + 1;
                     x++;
                 }
             }
             y++;
-            
         }
         arquivo.close();
     }
     return ums;
 }
 
-void arquivo_saida(vector<vector<int>> atq, string tx){
+void arquivo_saida(vector<vector<int>> atq, string tx) {
     ofstream arquivo("ataques.txt", ios::app);
-    arquivo<<tx<<endl;
-    for (int i = 0; i < atq.size() - 1; i=i+2){
+    arquivo << tx << endl;
+    for (int i = 0; i < atq.size() - 1; i=i+2) {
         arquivo << atq[i][0] << ',' << atq[i][1] <<' ';
         arquivo << atq[i+1][0] << ',' << atq[i+1][1] <<endl;
     }
-    arquivo<<'\n';
+    arquivo << '\n';
     arquivo.close();
 }
 
 bool ataques(int **queens, string tx) {
     vector<vector<int>> ameacas;
     for (int i = 0; i < 7; i++) {
-        for (int j = i+1; j<8;j++){
-            if (queens[i][0]==queens[j][0]) {
+        for (int j = i + 1; j < 8; j++) {
+            if (queens[i][0] == queens[j][0]) {
                 vector<int> aux;
                 vector<int> aux2;
                 aux.push_back(queens[i][0]);
@@ -87,23 +87,7 @@ bool ataques(int **queens, string tx) {
                 ameacas.push_back(aux2);
                 break;
             }
-            if (queens[i][1]==queens[j][1]) {
-                vector<int> aux;
-                vector<int> aux2;
-                aux.push_back(queens[i][0]);
-                aux.push_back(queens[i][1]);
-                ameacas.push_back(aux);
-                aux2.push_back(queens[j][0]);
-                aux2.push_back(queens[j][1]);
-                ameacas.push_back(aux2);
-                break;
-            }
-        }
-    }
-    for (int i = 0; i < 7; i++)
-    {
-        for (int j = i+1; j < 8; j++){
-            if (fabs(queens[i][1]-queens[j][1])/fabs(queens[i][0]-queens[j][0])==1) {
+            if (queens[i][1] == queens[j][1]) {
                 vector<int> aux;
                 vector<int> aux2;
                 aux.push_back(queens[i][0]);
@@ -116,17 +100,29 @@ bool ataques(int **queens, string tx) {
             }
         }
     }
-    if (ameacas.size()==0) {
+    for (int i = 0; i < 7; i++) {
+        for (int j = i + 1; j < 8; j++) {
+            if (fabs(queens[i][1] - queens[j][1])/fabs(queens[i][0] - queens[j][0]) == 1) {
+                vector<int> aux;
+                vector<int> aux2;
+                aux.push_back(queens[i][0]);
+                aux.push_back(queens[i][1]);
+                ameacas.push_back(aux);
+                aux2.push_back(queens[j][0]);
+                aux2.push_back(queens[j][1]);
+                ameacas.push_back(aux2);
+                break;
+            }
+        }
+    }
+    if (ameacas.size() == 0) {
         return false;
     }
     arquivo_saida(ameacas, tx);
     return true;
 }
 
-
-
-int chamada(string a){
-
+int chamada(string a) {
     if (formatacao(a)) {
         return -1;
     }
@@ -140,5 +136,4 @@ int chamada(string a){
     }
     free(rainhas);
   return 0;
-  
 }
--- rainhas.hpp
+++ rainhas.hpp
@@ -1,9 +1,49 @@
 #include <bits/stdc++.h>
 using namespace std;
+#include <vector>
+#include <string>
 
+/**
+ * @brief essa primeira funcao lê o arquivo e descobre se é 8x8
+ * e se tem exatas 8 rainhas
+ * 
+ * 
+ * A funçao lê linha por linha do arquivo e ja vai contando se
+ * se a linha é de 8 termos, após isso ele vai rodar a linha e 
+ * ver se há 1 nessa linha e aumentar a qntd_rainhas em 1;
+ * se algum desses for diferente de 8;
+ * @param teste o nome do arquivo;
+ * @return true se o tabuleiro não é 8 por 8 ou n tem 8 rainhas;
+ * @return false se o tabuleiro é 8x8 e tem 8 rainhas;
+ */
 bool formatacao(string teste);
 
+/**
+ * @brief Cria uma matriz de coordenadas das rainhas com base no arquivo de entrada.
+ * em que é contado em eixo x e y, sendo a linha 0 do arquivo a linha 1 do tabuleiro;
+ * @param arquivos o arquivo do tabuleiro;
+ * @return int retorna a uma matriz com a posicao de cada rainhas
+ */
 int chamada(string arquivos);
+
+/**
+ * Verifica se há ataques entre as rainhas.
+ * @param queens Matriz de coordenadas das rainhas.
+ * @param tx Nome do arquivo de entrada.
+ * @return Retorna verdadeiro se houver ataques entre as rainhas, falso caso contrário.
+ */
 bool ataques(int **queens, string tx);
+
+/**
+ * Salva os ataques entre rainhas em um arquivo de saída.
+ * @param atq O vetor com as coordenadas das rainhas que se atacam.
+ * @param tx Nome do arquivo de entrada.
+ */
 void arquivo_saida(vector<vector<int>> atq, string tx);
+
+/**
+ * Função principal que faz a chamada para verificar o arquivo de entrada.
+ * @param a Nome do arquivo de entrada.
+ * @return Retorna -1 se a formatação estiver incorreta, 1 se houver ataques entre as rainhas e 0 caso contrário.
+ */
 int** matriz(string teste);
