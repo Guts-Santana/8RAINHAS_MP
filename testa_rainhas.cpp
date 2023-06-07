b31cb6207d3533b8e9a0198277b80ecc1aba4d2d
GustavoSantana <211038235@aluno.unb.br>
2023-06-07 13:59:03 -0300

    teste7

--- testa_rainhas.cpp
+++ testa_rainhas.cpp
@@ -8,6 +8,9 @@ TEST_CASE("Se o arquivo tem 8 linhas") {
    REQUIRE(chamada("teste3.txt") == -1);
    REQUIRE(chamada("teste4.txt") == -1);
    REQUIRE(chamada("teste5.txt") == -1);
-//    REQUIRE(chamada("teste2.txt") == -1);
-//    REQUIRE(chamada("teste3.txt") == -1);
+}
+
+TEST_CASE("Se o arquivo só tem linhas de 8 termos"){
+   REQUIRE(chamada("teste6.txt") == -1);
+   REQUIRE(chamada("teste7.txt") == -1);
 }
